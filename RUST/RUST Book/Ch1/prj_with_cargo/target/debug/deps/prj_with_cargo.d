aba414b5bdc11d2223c939bdc2f862d83dc5a122
Aditya Singh <44133844+NyanAditya@users.noreply.github.com>
2024-10-16 16:30:24 +0530

    chore: add RUST/RUST Book/Ch1/prj_with_cargo

---
+++ RUST/RUST Book/Ch1/prj_with_cargo/target/debug/deps/prj_with_cargo.d
@@ -0,0 +1,5 @@
+D:\Code\CodeNomad\RUST\RUST Book\Ch1\prj_with_cargo\target\debug\deps\prj_with_cargo.exe: src/main.rs
+
+D:\Code\CodeNomad\RUST\RUST Book\Ch1\prj_with_cargo\target\debug\deps\prj_with_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ RUST/RUST Book/Ch1/prj_with_cargo/target/debug/prj_with_cargo.d
@@ -0,0 +1,1 @@
+D:\Code\CodeNomad\RUST\RUST\ Book\Ch1\prj_with_cargo\target\debug\prj_with_cargo.exe: D:\Code\CodeNomad\RUST\RUST\ Book\Ch1\prj_with_cargo\src\main.rs
