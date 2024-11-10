180c8ef37d6325d6d008d040a892b2cc21fa8862
Aditya Singh <44133844+NyanAditya@users.noreply.github.com>
2024-11-10 10:41:42 +0530

    cmd: cargo clean

--- RUST/target/debug/deps/RUST-4f271fbe84e8d2a3.d
+++
@@ -1,5 +0,0 @@
-C:\Users\nandk\OneDrive - nekoai\Documents\GitHub\CodeNomad\RUST\target\debug\deps\libRUST-4f271fbe84e8d2a3.rmeta: src/main.rs
-
-C:\Users\nandk\OneDrive - nekoai\Documents\GitHub\CodeNomad\RUST\target\debug\deps\RUST-4f271fbe84e8d2a3.d: src/main.rs
-
-src/main.rs:
--- RUST/target/debug/deps/RUST-5ea216814b915bf3.d
+++
@@ -1,5 +0,0 @@
-D:\Code\CodeNomad\RUST\target\debug\deps\libRUST-5ea216814b915bf3.rmeta: src/main.rs
-
-D:\Code\CodeNomad\RUST\target\debug\deps\RUST-5ea216814b915bf3.d: src/main.rs
-
-src/main.rs:
